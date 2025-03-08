626c1bb27c3621347a22508398f2abe1cf9372d3
MartinRR2710 <mrivera15759@plataforma-utslp.net>
2025-03-08 15:51:28 -0500

    p3

--- p3-1.sh
+++ p3-1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Declaramos un array con las URLs
 declare -a urls=(
     "https://www.facebook.com/"
@@ -11,17 +10,14 @@ declare -a urls=(
 file=url.csv
 
 # Bucle infinito para monitorear las URLs
-while true; do
     # Agregamos la fecha actual al archivo temporal
-    printf "$(date)\n" > "$file"
-
-    # Iteramos sobre cada URL
-    for url in "${urls[@]}"; do
-        # Obtenemos el código de estado HTTP con curl
-        status=$(curl -m 10 -s -I "$url" | head -n 1 | awk '{print $2}')
+printf "$(date)\n" > "$file"
+# Iteramos sobre cada URL
+for url in "${urls[@]}"; do
+# Obtenemos el código de estado HTTP con curl
+	status=$(curl -m 10 -s -I "$url" | head -n 1 | awk '{print $2}')
         # Guardamos la URL y el estado en el archivo temporal
-        printf "%s,%s\n" "$url" "$status" >> "$file"
-    done
-
-    # Mostramos la salida en formato tabular
-    column -s, -t "$file"
+        printf "%s$url,$status\n" >> "$file"
+done
+# Mostramos la salida en formato tabular
+column -s, -t "$file"
